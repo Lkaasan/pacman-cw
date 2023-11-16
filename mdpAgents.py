88607cbf3e8eae157f18d3d631efebd0195a9bf7
Lkaasan <49850026+Lkaasan@users.noreply.github.com>
2023-11-16 13:55:11 +0000

    Bellman equation and value iteration

--- mdpAgents.py
+++ mdpAgents.py
@@ -69,7 +69,7 @@ class MDPAgent(Agent):
 
     # For now I just move randomly
     def getAction(self, state):
-        v_iterations(self.map, state)
+        self.map = v_iterations(self.map, state)
         # Get the actions we can try, and remove "STOP" if that is one of them.
         legal = api.legalActions(state)
         if Directions.STOP in legal:
@@ -80,13 +80,66 @@ class MDPAgent(Agent):
 def v_iterations(map, state):
     reward_map = populateRewards(state)
     
-def bellmanEquation(currValue, currCell, xMax, yMax, map):
+    width = api.corners(state)[2][1] + 1
+    height = api.corners(state)[1][0] + 1
+    
+    for x in range(0, 10):
+        temp_map = initial_map(api.walls(state), api.corners(state))
+        for i in range(width):
+            for j in range(height):
+                current_cell_value = reward_map[i][j]
+                current_cell = (i, j)
+                temp_map[i][j] = bellmanEquation(current_cell_value, current_cell, width, height, reward_map)
+        reward_map = temp_map
+    return reward_map
+    
+def bellmanEquation(curr_value, currCell, width, height, map):
     x = currCell[0]
     y = currCell[1]
-    if currValue is None:
+    if curr_value is None:
         return None
+    e = None
+    w = None
+    n = None
+    s = None
     
+    if x < width - 1:
+        east = map[x + 1][y]
+    if x > 0:
+        west = map[x - 1][y]
+    if y < height - 1:
+        north = map[x][y + 1]
+    if y > 0:
+        south = map[x][y - 1]
+
+    if east is None:
+        east = - 1
+    if west is None:
+        west = -1
+    if north is None:
+        north = -1
+    if south is None:
+        south = -1
     
+    if north is not None:
+        north_val = north * 0.8 + (east + west) * 0.1
+    else:
+        north_val = current
+    if south is not None:
+        south_val = south * 0.8 + (east + west) * 0.1
+    else:
+        south_val = current
+    if east is not None:
+        east_val = east * 0.8 + (north + south) * 0.1
+    else:
+        east_val = current
+    if west is not None:
+        west_val = west * 0.8 + (north + south) * 0.1
+    else:
+        west_val = current
+        
+    max_val = max([north_val, south_val, east_val, west_val])
+    return float(float(curr_value) + float(GAMMA) * float(max_val))
     
 def populateRewards(state):
     f = api.food(state)
@@ -96,30 +149,31 @@ def populateRewards(state):
     ca = api.capsules(state)
     
     m = initial_map(w, co)
-    xMax = co[1][0] + 1
-    yMax = co[2][1] + 1
+    width = co[1][0] + 1
+    height = co[2][1] + 1
+    print (width, height)
     
-    for i in range(xMax):
-        for j in range(yMax):
+    for i in range(width):
+        for j in range(height):
             if (i, j) in f:
                 m[j][i] = FOOD_REWARD
             if (i, j) in g:
                 m[j][i] = GHOST_REWARD
             if (i, j) in ca:
                 m[j][i] = CAPSULE_REWARD 
-    print m
-    print
+    #print m
+    #print
                 
     return m
 
 def initial_map(walls, corners):
-    xMax = corners[1][0] + 1
-    yMax = corners[2][1] + 1
-    pacman_map = [[" " for x in range(xMax)] for y in range(yMax)]
+    width = corners[1][0] + 1
+    height = corners[2][1] + 1
+    pacman_map = [[" " for x in range(width)] for y in range(height)]
     
 
-    for i in range(xMax):
-        for j in range(yMax):
+    for i in range(width):
+        for j in range(height):
             if (i, j) in walls:
                 pacman_map[j][i] = None
             else:
