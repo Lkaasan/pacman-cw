339e990b7fcf586e0f677d453b1fe440de19d0e8
Lkaasan <49850026+Lkaasan@users.noreply.github.com>
2023-11-29 17:37:05 +0000

    Finished

--- mdpAgents.py
+++ mdpAgents.py
@@ -1,33 +1,3 @@
-# mdpAgents.py
-# parsons/20-nov-2017
-#
-# Version 1
-#
-# The starting point for CW2.
-#
-# Intended to work with the PacMan AI projects from:
-#
-# http://ai.berkeley.edu/
-#
-# These use a simple API that allow us to control Pacman's interaction with
-# the environment adding a layer on top of the AI Berkeley code.
-#
-# As required by the licensing agreement for the PacMan AI we have:
-#
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
-# The agent here is was written by Simon Parsons, based on the code in
-# pacmanAgents.py
-
 from pacman import Directions
 from game import Agent
 import api
@@ -37,12 +7,19 @@ import util
 import time
 
 
+"""
+Class to represent a cell in the map
+"""
 class Cell:
 
+    """
+    Constructor for the Cell class
+    @param value with type Double: reward value of the cell
+    @param coordinate with type Tupele (int, int)
+    """
     def __init__(self, value, coordinate):
         self.value = value
         self.coordinate = coordinate
-
 
 class MDPAgent(Agent):
 
@@ -50,6 +27,8 @@ class MDPAgent(Agent):
     def __init__(self):
         print "Starting up MDPAgent!"
         name = "Pacman" 
+        
+        #Initiallising class variables
         self.corners = self.walls = self.map = self.width = self.height = self.capsules = None
         self.food_reward = 10
         self.empty_reward = -0.04
@@ -64,43 +43,48 @@ class MDPAgent(Agent):
         print "Running registerInitialState for MDPAgent!"
         print "I'm at:"
         print api.whereAmI(state)
+        
+        #setting game parameters to class variables 
         self.corners = api.corners(state)
         self.walls = api.walls(state)
-        
         self.width = self.corners[1][0] + 1
         self.height = self.corners[2][1] + 1
         self.map = self.create_empty_map()
         
-        
-    # This is what gets run in between multiple games
+
     def final(self, state):
         self.map = None
         print "Looks like the game just ended!"
 
-    # For now I just move randomly
     def getAction(self, state):
         self.capsules = api.capsules(state)
         self.map = self.create_empty_map()
         self.populate_rewards(state)
         self.v_iteration(state)
+        
         legal = api.legalActions(state)
         if Directions.STOP in legal:
             legal.remove(Directions.STOP)
+            
         pacman_location = api.whereAmI(state)
-        [scores, actions] = self.get_action_scores(legal, self.map, pacman_location[0], pacman_location[1])
+        [scores, actions] = self.get_score_for_actions(legal, pacman_location[0], pacman_location[1])
+        
         if all(score == scores[0] for score in scores):
             self.move_towards_closest_food(legal, state)
-            [scores, actions] = self.get_action_scores(legal, self.map, pacman_location[0], pacman_location[1])
+            [scores, actions] = self.get_score_for_actions(legal, pacman_location[0], pacman_location[1])
             if all(score == scores[0] for score in scores):
                 return api.makeMove(random.choice(legal), legal)
-        # print scores
-        # print actions
+            
         max_score_index = scores.index(max(scores))
         choice = actions[max_score_index]
-        
-        # print choice
         return api.makeMove(choice, legal)
     
+    """
+        Function that alters the map rewards when pacman is stuck with no path, to help move
+        him towards the nearest food
+        @param legal of type list: a list of all legal moves 
+        @param state, the state of the game
+    """
     def move_towards_closest_food(self, legal, state):
         food = api.food(state)
         pacman = api.whereAmI(state)
@@ -121,7 +105,12 @@ class MDPAgent(Agent):
             if self.map[pacman[1] - 1][pacman[0]] is not None:
                 self.map[pacman[1] - 1][pacman[0]] += 5
 
-        
+    """
+    Finds the closest food to pacman at the current state of the game
+    @param pacman of type tuple [int, int]: the location of pacman
+    @param food of type list: a list of food in the current game state
+    @return closest_food of type tuple: location of closest food
+    """
     def find_closest_food(self, pacman, food):
         distance = 100
         closest_food = ()
@@ -132,26 +121,37 @@ class MDPAgent(Agent):
                 closest_food = f
         return closest_food
 
-    def get_action_scores(self, legal, pacman_map, x, y):
-        action_scores = {}
-        for action in legal:
-            value = None
-            if action is Directions.NORTH:
-                value = pacman_map[y + 1][x]
-            elif action is Directions.SOUTH:
-                value = pacman_map[y - 1][x]
-            elif action is Directions.EAST:
-                value = pacman_map[y][x + 1]
-            elif action is Directions.WEST:
-                value = pacman_map[y][x - 1]
-            if value is not None:
-                action_scores[action] = value
-
-        scores = list(action_scores.values())
-        actions = list(action_scores.keys())
+    """
+    Gets the actions pacman can take and the score of those actions
+    @param legal of type list: a list of all legal moves
+    @param x of type int: x coordinate
+    @param y of type int: y coordinate
+    @return scores of type list: list of scores
+    @return actions of type list: list of actions
+    """
+    def get_score_for_actions(self, legal, x, y):
+        scores = []
+        actions = []
+        for a in legal:
+            if a is Directions.NORTH:
+                scores.append(self.map[y + 1][x])
+                actions.append(a)
+            elif a is Directions.SOUTH:
+                scores.append(self.map[y - 1][x])
+                actions.append(a)
+            elif a is Directions.EAST:
+                scores.append(self.map[y][x + 1])
+                actions.append(a)
+            elif a is Directions.WEST:
+                scores.append(self.map[y][x - 1])
+                actions.append(a)
 
         return scores, actions
 
+    """
+    Implments value iteration of the map for 10 iterations
+    @param state: current state of the game
+    """
     def v_iteration(self, state):
         if self.height == 7:
            self.small_map_update_ghost_values(state)
@@ -167,6 +167,11 @@ class MDPAgent(Agent):
             self.map = empty_map
         
 
+    """
+    Updates the reward values for the space around the ghost, negative if ghost is normal 
+    and posive if ghost is scared
+    @param state: current state of the game
+    """
     def update_ghost_rewards(self, state): 
         ghosts = api.ghosts(state)
         ghosts_with_states = api.ghostStates(state)
@@ -182,33 +187,38 @@ class MDPAgent(Agent):
                     else:
                         self.map[y][x] = self.scared_ghost_reward / 2
             counter += 1
-             
+    
+    
+    """
+    Updates the reward values for the space around the ghost on the small map
+    @param state: current state of the game 
+    """
     def small_map_update_ghost_values(self, state):
         ghosts = api.ghosts(state)
         ghosts_with_states = api.ghostStates(state)
         pacman_location = api.whereAmI(state)
         pacman_neighbours = self.neighbours(pacman_location, 0)
-        counter = 0
         for ghost in ghosts:
             neighbours = self.neighbours(ghost, 0)
             for n in neighbours:
                 x = int(n[0])
                 y = int(n[1])
                 if self.map[y][x] is not None:
-                    if n in pacman_neighbours:
-                        self.map[y][x] -= (2000)
-                    else:
-                        self.map[y][x] -= (1000 / self.distance_to_ghost(state, ghost))
-        counter += 1
+                    self.map[y][x] = self.ghost_reward / 2
             
-        
+    """
+    Implements the bellmann equation of a cell
+    @param c type Cell: the cell
+    @param m of type 2D Array: the current map
+    @return new value of the cell, type float
+    """
     def bellmann_equation(self, c, m):
-        x, y = c.coordinate
+        x, y = c.coordinate 
 
         if c.value is None:
             return None
 
-        neighbors = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+        neighbors = self.neighbours(c.coordinate, 0)
         valid_neighbors = [(nx, ny) for nx, ny in neighbors if 0 <= nx < self.width and 0 <= ny < self.height]
 
         values = [m[nx][ny] if m[nx][ny] is not None else -1 for nx, ny in valid_neighbors]
@@ -216,9 +226,13 @@ class MDPAgent(Agent):
         weighted_values = [0.8 * v for v in values]
         weighted_values.append(0.1 * c.value)  
 
-        max_val = max(weighted_values)
-        return round((c.value + self.gamma * max_val), 2)
+        m_val = max(weighted_values)
+        return round((c.value + self.gamma * m_val), 2)
 
+    """
+    Populate the initial reward values of the map depending on the game constrants
+    @param state: current state of the game
+    """
     def populate_rewards(self, state):
         ghosts = api.ghosts(state)
         food = api.food(state)
@@ -241,6 +255,10 @@ class MDPAgent(Agent):
                 elif (i, j) == (9, 6) or (i, j) == (10, 6):
                     self.map[j][i] = -1000
         
+    """
+    Creates an empty map, only with empy cells and walls
+    @return p_map: empty map
+    """
     def create_empty_map(self):
         p_map = [[" " for i in range(self.width)] for j in range(self.height)]
         for i in range(self.width):
@@ -250,10 +268,12 @@ class MDPAgent(Agent):
                 else: 
                     p_map[j][i] = self.empty_reward
         return p_map
-
-    def distance_to_ghost(self, state, ghost):
-        return util.manhattanDistance(api.whereAmI(state), ghost)
-    
+    """
+    Returns the neighbouring cells of a location, can either be a small size of large size
+    @param location of type tuple (int, int): location for neighbours to be found
+    @param t of type int: 0 for small range and 1 for large range
+    @return neighbours of type list: a list of neighbouring cells
+    """
     def neighbours(self, location, t):
         neighbours = []
         x = location[0] 
@@ -266,6 +286,7 @@ class MDPAgent(Agent):
             neighbours.append((x + 1, y))
         if x > 0:
             neighbours.append((x - 1, y))
+            
         if t == 1:
             if y < self.height - 2:
                 neighbours.append((x, y + 2))
@@ -284,16 +305,6 @@ class MDPAgent(Agent):
                 neighbours.append((x + 1, y - 1))
             if x > 0 and y > 0:
                 neighbours.append((x - 1, y - 1))
-                
+
         return neighbours
-            
-    def prettyDisplay(self):       
-        for i in range(self.height):
-            for j in range(self.width):
-                # print grid elements with no newline
-                print self.map[self.height - (i + 1)][j],
-            # A new line after each line of the grid
-            print 
-        # A line after the grid
-        print
         
